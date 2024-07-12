223151ce22edbd0e2837671ddf77241eed9a1625
AnalyticalAscent <william.carter.analytics@gmail.com>
2024-07-12 14:25:27 -0400

    Adding Part 6 Files.

---
+++ Learning_Python_Part_6_My_Module.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    """This function returns the sum of two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """This function returns the difference of two numbers."""
+    return a - b
